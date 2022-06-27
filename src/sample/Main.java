1d2c12f9466c86eb240860ff77259f310c79e196
alexjyoung6 <95151615+alexjyoung6@users.noreply.github.com>
2022-06-26 20:35:11 -0500

    Updated

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -95,16 +95,16 @@ public class Main extends Application {
             grid.setHgap(10);
 
 ///////////////////////////////////////////////////////////////////////Enter Doctor's Name
-            Label logInLabel = new Label("LOG IN SUCCESSFUL:");
+            Label logInLabel = new Label("LOG IN SUCCESSFUL");
             GridPane.setConstraints(logInLabel, 10, 3);
 
             Label nameLabel = new Label("Enter Doctor's Name:");
-            GridPane.setConstraints(nameLabel, 14, 4);
+            GridPane.setConstraints(nameLabel, 10, 4);
             TextField doctorNameText = new TextField();
             doctorNameText.setMinSize(100, 45);
-            GridPane.setConstraints(doctorNameText, 16, 4);
+            GridPane.setConstraints(doctorNameText, 15, 5);
 
-//////////////////////////////////////////////////////////////////////////////Button
+//////////////////////////////////////////////////////////////////////////////Button to enter doctors name
             Button enterDoctorNameButton = new Button("ENTER");
             enterDoctorNameButton.setStyle("-fx-background-color: MediumSeaGreen");
             enterDoctorNameButton.setMinSize(60, 45);
@@ -118,11 +118,71 @@ public class Main extends Application {
 
                 }
             });
+            grid.getChildren().addAll(logInLabel,nameLabel,doctorNameText,enterDoctorNameButton);
+            Scene scene = new Scene(grid, 700, 700);
+            stage.setScene(scene);
+            stage.show();
 
         }
+        /////////////////////////////////////////////////////////////////////STAGE 2 --> Make Appointment Use Case/////////////////////////////////////////////////////
         else if(newStage == 2){ //Enter Patient name, date and time
+            stage.setTitle("Health-Care System");
+            GridPane grid = new GridPane();
+            grid.setPadding(new Insets(25, 25, 25, 25));
+            grid.setVgap(18);
+            grid.setHgap(12);
+
+            Label nameDateTimeLabel = new Label("Enter Name, Date, and Time");
+            GridPane.setConstraints(nameDateTimeLabel, 10, 2);
+            ////////////////////////////////////////////////////////////////////////Display doctor available date and time
+            Label availableDateLabel = new Label("Display available dates");
+            GridPane.setConstraints(availableDateLabel, 5, 3);
+            Label availableTimeLabel = new Label("Display available times");
+            GridPane.setConstraints(availableTimeLabel, 5, 4);
+//////////////////////////////////////////////////////////////////////////////////////Enter the patients name
+            Label patientNameLabel = new Label("Enter Patient Name:");
+            GridPane.setConstraints(patientNameLabel, 10, 3);
+            TextField patientNameText = new TextField();
+            patientNameText.setMinSize(100, 45);
+            GridPane.setConstraints(patientNameText, 10, 4);
+
+//////////////////////////////////////////////////////////////////////////////////////Enter the day --> Monday,Tuesday,Wednesday,Thursday,Friday
+            Label selectDateLabel = new Label("Select Date(M,T,W,TH,F):");
+            GridPane.setConstraints(selectDateLabel, 10, 5);
+            TextField enterDateText = new TextField();
+            enterDateText.setMinSize(100, 45);
+            GridPane.setConstraints(enterDateText, 10, 6);
+
+///////////////////////////////////////////////////////////////////////////////////////Enter the time
+
+            Label selectTimeLabel = new Label("Select Time:");
+            GridPane.setConstraints(selectTimeLabel, 10, 7);
+            TextField enterTimeText = new TextField();
+            enterTimeText.setMinSize(100, 45);
+            GridPane.setConstraints(enterTimeText, 10, 8);
+
+/////////////////////////////////////////////////////////////////////////////////////////////Button
+            Button makeAppointmentButton = new Button("Make Appointment");
+            makeAppointmentButton.setStyle("-fx-background-color: MediumSeaGreen");
+            makeAppointmentButton.setMinSize(70, 45);
+            GridPane.setConstraints(makeAppointmentButton, 10, 9);
+            makeAppointmentButton.setOnAction(new EventHandler<ActionEvent>() {
+                @Override
+                public void handle(ActionEvent actionEvent) {
+                    String Temp = makeAppointmentButton.getText();
+
+                    storeAppointment(Temp);
+
+                }
+            });
+////////////////////////////////////////////////////////////////////////////////////////////
+            grid.getChildren().addAll(nameDateTimeLabel, patientNameLabel, patientNameText, selectDateLabel, enterDateText,selectTimeLabel,enterTimeText,makeAppointmentButton,availableDateLabel, availableTimeLabel);
+            Scene scene = new Scene(grid, 800, 800);
+            stage.setScene(scene);
+            stage.show();
 
         }
+
 
 
 
@@ -136,9 +196,9 @@ public class Main extends Application {
         String database_address = "jdbc:oracle:thin:@localhost:1521:xe";
         String database_username = "system";
         String database_password = "YellowGreen27";
-        Connection conn = DriverManager.getConnection(database_address, database_username, database_password);
+       // Connection conn = DriverManager.getConnection(database_address, database_username, database_password);
 
-        DatabaseAccess.verify_user_pass(conn,nameTemp,IDTemp);
+        //DatabaseAccess.verify_user_pass(conn,nameTemp,IDTemp);
 
         try {
             changeStage(primaryStage,1);
@@ -155,6 +215,9 @@ public class Main extends Application {
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         }
+
+    }
+    public void storeAppointment(String temp){
 
     }
 
